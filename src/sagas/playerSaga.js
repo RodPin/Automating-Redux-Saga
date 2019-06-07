bf103d5da23e608193291e5a5d2e6f95ac45e66c
Rodrigo Gomes <prodrigo@poli.ufrj.br>
2019-06-07 09:58:11 -0300

    Calling sagas inside sagas

--- src/sagas/playerSaga.js
+++ src/sagas/playerSaga.js
@@ -1,9 +1,10 @@
-import { put, call } from "redux-saga/effects";
-import { types } from "../reducers/playerReducer";
-import { setShirtSaga } from "./shirtSaga";
-import { addSaga } from "./counterSaga";
+import { put } from "redux-saga/effects";
+import { types as counterTypes } from "../reducers/counterReducer";
+import { types as playerTypes } from "../reducers/playerReducer";
+import { types as shirtTypes } from "../reducers/shirtReducer";
+
 export function* setPlayerSaga(action) {
-  yield put({ type: types.SET_PLAYER, payload: action.payload });
-  yield call(setShirtSaga, action.payload);
-  yield call(addSaga);
+  yield put({ type: playerTypes.SET_PLAYER, payload: action.payload });
+  yield put({ type: shirtTypes.SET_SHIRT_SAGA, payload: action.payload });
+  yield put({ type: counterTypes.ADD_SAGA });
 }
--- src/sagas/shirtSaga.js
+++ src/sagas/shirtSaga.js
@@ -2,5 +2,5 @@ import { put } from "redux-saga/effects";
 import { types } from "../reducers/shirtReducer";
 
 export function* setShirtSaga(action) {
-  yield put({ type: types.SET_SHIRT, payload: action });
+  yield put({ type: types.SET_SHIRT, payload: action.payload });
 }
